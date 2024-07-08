e28e5f96a05956189d4f51e0172a6323a38870ac
EltajPirverdiyev <eltacc7@gmail.com>
2024-07-08 09:52:24 +0300

    Add project files.

---
+++ yer istasyonu/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace yer_istasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCamera1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.btnCamera2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnStop1 = new System.Windows.Forms.Button();
+            this.btnStop2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCamera1
+            // 
+            this.btnCamera1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCamera1.Location = new System.Drawing.Point(25, 42);
+            this.btnCamera1.Name = "btnCamera1";
+            this.btnCamera1.Size = new System.Drawing.Size(160, 28);
+            this.btnCamera1.TabIndex = 0;
+            this.btnCamera1.Text = "Start Camera1";
+            this.btnCamera1.UseVisualStyleBackColor = true;
+            this.btnCamera1.Click += new System.EventHandler(this.btnCamera1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(196, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(186, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(475, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.BorderSize = 5;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(37, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 99);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Target Detection Automatic";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.FlatAppearance.BorderSize = 5;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(37, 490);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 99);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Target Tracking Automatic";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button5.FlatAppearance.BorderSize = 5;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(187, 373);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 99);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Target Destruction Automatic";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button6.FlatAppearance.BorderSize = 5;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(187, 490);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 99);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Emergency Button";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button7.FlatAppearance.BorderSize = 5;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(346, 373);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(173, 70);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Target Detection Manual";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button8.FlatAppearance.BorderSize = 5;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(346, 449);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(173, 64);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Target Tracking Manual";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button9.FlatAppearance.BorderSize = 5;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(346, 519);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(173, 70);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Target Destruction Manual";
+            this.button9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.White;
+            this.button10.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.button10.FlatAppearance.BorderSize = 5;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.Red;
+            this.button10.Location = new System.Drawing.Point(540, 373);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(108, 216);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "FIRE";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.White;
+            this.button11.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button11.FlatAppearance.BorderSize = 5;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(668, 373);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(224, 99);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "Fully Automatic Mode";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.White;
+            this.button12.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button12.FlatAppearance.BorderSize = 5;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(668, 490);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(224, 99);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "Full Manual Mode";
+            this.button12.UseVisualStyleBackColor = false;
+            // 
+            // btnCamera2
+            // 
+            this.btnCamera2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCamera2.Location = new System.Drawing.Point(512, 46);
+            this.btnCamera2.Name = "btnCamera2";
+            this.btnCamera2.Size = new System.Drawing.Size(163, 28);
+            this.btnCamera2.TabIndex = 16;
+            this.btnCamera2.Text = "Start Camera2";
+            this.btnCamera2.UseVisualStyleBackColor = true;
+            this.btnCamera2.Click += new System.EventHandler(this.btnCamera2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(692, 50);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(186, 24);
+            this.comboBox2.TabIndex = 17;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(512, 76);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(475, 291);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnStop1
+            // 
+            this.btnStop1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop1.Location = new System.Drawing.Point(399, 46);
+            this.btnStop1.Name = "btnStop1";
+            this.btnStop1.Size = new System.Drawing.Size(91, 28);
+            this.btnStop1.TabIndex = 19;
+            this.btnStop1.Text = "Stop";
+            this.btnStop1.UseVisualStyleBackColor = true;
+            this.btnStop1.Click += new System.EventHandler(this.btnStop1_Click);
+            // 
+            // btnStop2
+            // 
+            this.btnStop2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop2.Location = new System.Drawing.Point(896, 50);
+            this.btnStop2.Name = "btnStop2";
+            this.btnStop2.Size = new System.Drawing.Size(91, 28);
+            this.btnStop2.TabIndex = 20;
+            this.btnStop2.Text = "Stop";
+            this.btnStop2.UseVisualStyleBackColor = true;
+            this.btnStop2.Click += new System.EventHandler(this.btnStop2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 603);
+            this.Controls.Add(this.btnStop2);
+            this.Controls.Add(this.btnStop1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.btnCamera2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnCamera1);
+            this.Name = "Form1";
+            this.Text = "GROUND STATION";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCamera1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button btnCamera2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnStop1;
+        private System.Windows.Forms.Button btnStop2;
+    }
+}
+
---
+++ yer istasyonu/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+
+namespace yer_istasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection fic;
+        VideoCaptureDevice vcd;
+        VideoCaptureDevice vcd1;
+
+        private void btnCamera1_Click(object sender, EventArgs e)
+        {
+            vcd = new VideoCaptureDevice(fic[comboBox1.SelectedIndex].MonikerString);
+            vcd.NewFrame += VideoCaptureDevice_NewFrame;
+            vcd.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fic = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in fic)
+            {
+                comboBox1.Items.Add(filterInfo.Name);
+                comboBox2.Items.Add(filterInfo.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            vcd = new VideoCaptureDevice();
+        }
+        private void btnCamera2_Click(object sender, EventArgs e)
+
+        {
+            vcd1 = new VideoCaptureDevice(fic[comboBox2.SelectedIndex].MonikerString);
+            vcd1.NewFrame += VideoCaptureDevice_NewFrame1;
+            vcd1.Start();
+        }
+        private void VideoCaptureDevice_NewFrame1(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            bitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            pictureBox2.Image = bitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(vcd.IsRunning == true)
+            {
+                vcd.Stop();
+            }
+            if (vcd1.IsRunning == true)
+            {
+                vcd1.Stop();
+            }
+        }
+
+        private void btnStop1_Click(object sender, EventArgs e)
+        {
+            if (vcd != null && vcd.IsRunning)
+            {
+                vcd.SignalToStop();
+                vcd.WaitForStop();
+                pictureBox1.Image = null; // pictureBox1'i temizleme (isteğe bağlı)
+            }
+        }
+
+        private void btnStop2_Click(object sender, EventArgs e)
+        {
+            if (vcd1 != null && vcd1.IsRunning)
+            {
+                vcd1.SignalToStop();
+                vcd1.WaitForStop();
+                pictureBox2.Image = null; // pictureBox1'i temizleme (isteğe bağlı)
+            }
+        }
+    
+    }
+}
